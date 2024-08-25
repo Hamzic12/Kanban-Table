6756509249f9b3ba13ce4753be086455b899948f
unknown <hamza23@seznam.cz>
2024-08-25 04:31:07 +0200

    concept

--- Back-end.py
+++ Back-end.py
@@ -1,11 +1,26 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+
+class Grid(GridLayout): # create 4 grid layouts and one stack layout
+    def __init__(self, **kwargs):
+        super(Grid, self).__init__(**kwargs)
+        self.cols = 4 # stages
+        self.rows = 10 # tasks
+        self.add_widget(Label(text="New task"))
+        self.add_widget(Widget())
+        self.task = TextInput(multiline=False) 
+        self.add_widget(self.task)
+
+
 
 class Kanban(App):
 
     def build(self):
-        return Label(text="Hello")
+        return Grid()
     
 if __name__ == "__main__":
     Kanban().run()
