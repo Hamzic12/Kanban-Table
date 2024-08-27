1348602f9f41f9c497ed389072a8ddba0983757f
unknown <hamza23@seznam.cz>
2024-08-27 03:01:37 +0200

    almost there

--- Back-end.py
+++ Back-end.py
@@ -4,14 +4,23 @@ from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.uix.label import Label
 from kivy.properties import StringProperty
+from kivy.properties import NumericProperty
 
-class Grid(Widget): # create 4 grid layouts and one stack layout
+class app_layout(Widget): # create grid layout of 1 grid layout 1 relative 1 grid and next is stack layout 
     task = ObjectProperty(None)
     sign = ObjectProperty(None)
     b = 0
+
+    # completion bar made up of 4 quarters
+    q1 = NumericProperty(0)
+    q2 = NumericProperty(0)
+    q3 = NumericProperty(0)
+    q4 = NumericProperty(0)
+    list_qs = [q1, q2, q3, q4]
+    
     variable = StringProperty("HELLO Variable")
     
-    def pressed(self):
+    """  def pressed(self):
         position = self.task.pos
         text = self.task.text
         print(position)
@@ -27,26 +36,28 @@ class Grid(Widget): # create 4 grid layouts and one stack layout
         self.add_widget(Label(text="Name: Jeff"))
         exampl = Label(text="Name: Jefssssssssssss")
 
-        self.add_widget(exampl)
-        exampl_pos = exampl.pos
+        exampl_pos = exampl.pos #getting position
 
 
-        exampl_pos[0] += 200 + self.b
+        exampl_pos[0] += 200 + self.b # changing position
         exampl_pos[1] += 200 + self.b
         
         self.b += 50
         
-        self.task.text = ""
-        self.variable = "after click variable"
+        self.task.text = "" # empty input field
+        self.variable = "after click variable" # changing label text
+        """
 
-    def remove(self):
-        #remove text from one label and change into another label
-        pass
+    def press_me(self):
+       for q in self.list_qs:
+           q += 1
+
+
 
 class Kanban(App):
 
     def build(self):
-        return Grid()
+        return app_layout()
     
 if __name__ == "__main__":
     Kanban().run()
